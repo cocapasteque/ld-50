20f5104d0909580a609390097f1978b28dfe911b
Stefan Laimer <s.laimer@str8labs.com>
2022-04-04 21:24:26 +0200

    Leaderboard preparations

---
+++ Assets/Scripts/RankingManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RankingManager : MonoBehaviour
+{
+    public Rank[] TopRanks;
+    public Rank PersonalRank;
+}
